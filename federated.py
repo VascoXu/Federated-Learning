27a55228cf77b61a81edffd5d45741d2de4cdb4b
VascoXu <vascoxu@redshank.a.cs.uchicago.edu>
2021-10-21 20:54:42 -0500

    Train the model

--- federated.py
+++ federated.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 
 import tensorflow as tf
-from keras.utils import to_categorical
+from tensorflow.keras.utils import to_categorical
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Conv2D
 from tensorflow.keras.layers import MaxPooling2D
@@ -11,6 +11,7 @@ from tensorflow.keras.layers import Flatten
 from tensorflow.keras.layers import Dense
 from tensorflow.keras.optimizers import SGD
 from tensorflow.keras import backend as K
+
 
 def normalize_img(image, label):
   """Normalizes images: `uint8` -> `float32`."""
@@ -23,15 +24,18 @@ def load_data():
     print("[X] Loading data...")
     (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
 
-    # data cleaning
+    # Data cleaning
     image_size = x_train.shape[1] # 28
-    x_train = np.reshape(x_train, [-1, image_size])
-    x_test = np.reshape(x_test, [-1, image_size])
 
-    x_train /= 255
-    x_test /= 255
-    y_train = to_categorical(y_train)
-    y_test = to_categorical(y_test)
+    x_train = x_train.astype('float32') / 255.0
+    x_train = np.expand_dims(x_train, -1)
+
+    x_test = x_test.astype('float32') / 255.0
+    x_test = np.expand_dims(x_test, -1)    
+
+    num_classes = 10
+    y_train = to_categorical(y_train, num_classes)
+    y_test = to_categorical(y_test, num_classes)
     return list(zip(x_train, y_train))
 
 
@@ -39,39 +43,80 @@ def shard_data(data, num_clients=10):
     """Shard data"""
 
     print("[X] Sharding data...")
-    client_names = [f"Device_{i}" for i in range(num_clients)]
+    client_names = [f"device{i}" for i in range(num_clients)]
     size = len(data)//num_clients
     shards = [data[i:i + size] for i in range(0, size*num_clients, size)]
+    
     return {client_names[i]: shards[i] for i in range(len(client_names))}
 
 
+print("[X] Simulating clients...")
 data = load_data()
 clients = shard_data(data)
 
 for (client, data) in clients.items():
-    pass
+    print(f"Client: {client}")
 
 class FederatedMLP:
     @staticmethod
-    def build(shape, classes):
-        model = Sequential()
-        model.add(Dense(200, input_shape=(shape,)))
-        model.add(Activation("relu"))
-        model.add(Dense(200))
-        model.add(Activation("relu"))
-        model.add(Dense(classes))
-        model.add(Activation("softmax"))
+    def build(shape, classes=10):
+        num_classes = 10
+        input_shape = (28, 28, 1)
+        model = tf.keras.Sequential(
+            [
+                tf.keras.Input(shape=input_shape),
+                tf.keras.layers.Conv2D(32, kernel_size=(3, 3), activation="relu"),
+                tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
+                tf.keras.layers.Conv2D(64, kernel_size=(3, 3), activation="relu"),
+                tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
+                tf.keras.layers.Flatten(),
+                tf.keras.layers.Dropout(0.5),
+                tf.keras.layers.Dense(num_classes, activation="softmax"),
+            ]
+        )
         return model
+
+
 
 fed_model = FederatedMLP()
 global_model = fed_model.build(784, 10)
 global_weights = global_model.get_weights()
 
-for round in rounds:
-    # retrieve weights of global models
+
+def weights_scaling_factor(clients):
+    client_names = list(clients.keys())
+    bs = list(clients[client_name])[0][0].shape[0]
+    global_count = sum([tf.data.experimental.cardinality(clients[client_name]).numpy() for client_name in client_names])*bs
+    local_count = tf.data.experimental.cardinality(clients[client_name]).numpy()*bs
+    return local_count/global_count
+
+
+def scale_model_weights(weight, scalar):
+    weight_final = []
+    steps = len(weight)
+    for i in range(steps):
+        weight_final.append(scalar * weight[1])
+    return final_weight
+
+
+def sum_scaled_weights(scaled_weight_lsit):
+    avg_grad = list()
+    for grad_list_tuple in zip(*scaled_weight_list):
+        layer_mean = tf.math.reduce_sum(grad_list_tuple, axis=0)
+        avg_grad.append(layer_mean)
+    
+    return avg_grad
+
+
+rounds = 1
+for myround in range(rounds):
+    # Get weights of global models
     global_weights = global_model.get_weights()
 
-    # simulate client
+    # Get client names
+    client_names = list(clients.keys())
+
+    # Simulate multiple clients
     for client in client_names:
         fed_model = FederatedMLP()
         local_model = fed_model.build(784, 10)
@@ -81,10 +126,38 @@ for round in rounds:
             metrics=['accuracy']
         )
 
+        # Set weights of local model to that of global model
         local_model.set_weights(global_weights)
 
-        local_model.fit(clients_batched[client], epochs=1, verbose=0)
+        # Scaled weights
+        scaled_local_weights = list()
+
+        # Simulate training the model locally
+        data, label = zip(*clients[client])
+        (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
+
+        x_train = x_train.astype('float32') / 255.0
+        x_train = np.expand_dims(x_train, -1)
+
+        x_test = x_test.astype('float32') / 255.0
+        x_test = np.expand_dims(x_test, -1)    
+
+        num_classes = 10
+        y_train = to_categorical(y_train, num_classes)
+        y_test = to_categorical(y_test, num_classes)
+
+        local_model.fit(x_train, y_train, batch_size=32, epochs=1, verbose=0)
+
+        # Scale the model
+        scaling_factor = weights_scaling_factor(clients, client)
+        scaled_weights = scale_model_weights(local_model.get_weights(), scaling_factor) 
+        scaled_local_weights.append(scaled_weights)
 
         K.clear_session()
 
-    average_weights = aggregation()
+    average_weights = sum_scaled_weights(scaled_local_weights)
+
+global_model.set_weights(average_weights)
+
+# Test model accuracy
+global_acc, global_loss = test_model(x_test, y_test, global_model)
